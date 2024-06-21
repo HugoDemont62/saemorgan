41f3659f26fae19b3f53af5371f880cb7b2069d1
nagrom <nagrom.martin@gmail.com>
2024-06-21 17:02:33 +0200

    ajout liste de fichier disponible correction des messages et des commentaires

--- airdrop/client2/client2.py
+++ airdrop/client2/client2.py
@@ -7,48 +7,51 @@ def client_program():
     echo_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        #  recuperer le nom du client
-        nom = input("Indiquez votre nom: ").strip()
-        # se connecter au serveur
+        #  Permet de récuperer le nom du client
+        nom = input("enter your name: ").strip()
+        # Permet de se connecter au serveur
         echo_socket.connect((host_name, port_number))
         connection_message = echo_socket.recv(1024).decode('utf-8').strip()
-        # envoyer le nom du client
+        # Permet d'envoyer le nom du client au serveur
         echo_socket.sendall(nom.encode('utf-8'))
         server_message = echo_socket.recv(1024).decode('utf-8').strip()
 
         while True:
-            # envoyer le choix au serveur
-            choice = input("0 télécharger 1 uploader 2 deconnecter 3 liste personnes connectées: ").strip()
+            # Envoie le choix au serveur
+            choice = input("do:download up:upload di:disconnect cl:client list fl:file list --> ").strip()
             echo_socket.sendall(choice.encode('utf-8'))
-            if choice == "0":
+            if choice == "do":
                 receive_file(echo_socket)
 
-            elif choice == "1":
+            elif choice == "up":
                 upload_file(echo_socket)
 
-            elif choice == "2":
-                print("deconnecté")
+            elif choice == "di":
+                print("disconnected")
                 echo_socket.close()
                 break
 
-            elif choice == "3":
+            elif choice == "cl":
                 server_message = echo_socket.recv(1024).strip()
-                print(f"Server: {server_message}")
+                print(f"server: {server_message}")
 
+            elif choice == "fl":
+                server_message = echo_socket.recv(1024).strip()
+                print(f"server: {server_message}")
             else:
-                print("Choix invalide.")
+                print("invalid choice")
 
     # Gère les erreurs
     except Exception as e:
-        print(f"Error: {e}")
+        print(f"error: {e}")
 
 
-# Gère la réception de fichiers
+# Gère la réception des fichiers
 def receive_file(echo_socket):
-    filename = input("Indiquez le nom du fichier: ").strip()
+    filename = input("enter file name: ").strip()
     echo_socket.sendall(filename.encode('utf-8'))
     filename_message = echo_socket.recv(1024).decode('utf-8').strip()
-    if filename_message.startswith("ENVOI"):
+    if filename_message.startswith("send"):
         _, filename, file_size = filename_message.split()
         file_size = int(file_size)
         with open(filename, 'wb') as file:
@@ -60,17 +63,17 @@ def receive_file(echo_socket):
                 file.write(data)
                 bytes_received += len(data)
 
-        print(f"File {filename} received successfully.")
+        print(f"file {filename} received successfully.")
     else:
-        print(f"Server: {filename_message}")
+        print(f"server: {filename_message}")
 
 
 # Gère l'envoie de fichiers
 def upload_file(echo_socket):
-    filename = input("Indiquez le nom du fichier à uploader: ").strip()
+    filename = input("Enter the file name to upload: ").strip()
     echo_socket.sendall(filename.encode('utf-8'))
     server_response = echo_socket.recv(1024).decode('utf-8').strip()
-    if server_response.startswith("READY TO RECEIVE"):
+    if server_response.startswith("ready to receive"):
         file_size = os.path.getsize(filename)
         echo_socket.sendall(str(file_size).encode('utf-8'))
         with open(filename, 'rb') as file:
@@ -79,9 +82,9 @@ def upload_file(echo_socket):
                 if not data:
                     break
                 echo_socket.sendall(data)
-        print(f"File {filename} sent successfully.")
+        print(f"file {filename} sent successfully.")
     else:
-        print(f"Server: {server_response}")
+        print(f"server: {server_response}")
 
 
 if __name__ == "__main__":
--- airdrop/server/server.py
+++ airdrop/server/server.py
@@ -5,6 +5,8 @@ from datetime import datetime
 
 listnom = []
 listip = []
+listfichier = []
+
 
 class EchoServerThread(threading.Thread):
     def __init__(self, client_socket, client_address):
@@ -14,28 +16,28 @@ class EchoServerThread(threading.Thread):
         self.messagelog = None
         self.filename = None
         self.nom = None
-        print(f"Connection from {client_address}")
+        print(f"connection from {client_address}")
 
-    # Définit la fonction envoie de fichier
+    # Défini la fonction envoie de fichier
     def send_file(self):
-        filename = self.client_socket.recv(1024).decode('utf-8').strip()
-        if os.path.exists(filename):
-            file_size = os.path.getsize(filename)
-            self.client_socket.sendall(f"ENVOI {filename} {file_size}".encode('utf-8'))
-            with open(filename, 'rb') as file:
+        self.filename = self.client_socket.recv(1024).decode('utf-8').strip()
+        if os.path.exists(self.filename):
+            file_size = os.path.getsize(self.filename)
+            self.client_socket.sendall(f"send {self.filename} {file_size}".encode('utf-8'))
+            with open(self.filename, 'rb') as file:
                 while True:
                     data = file.read(1024)
                     if not data:
                         break
                     self.client_socket.sendall(data)
-            print(f"File {filename} sent successfully.")
+            print(f"File {self.filename} sent successfully.")
         else:
-            self.client_socket.sendall("Le fichier n'existe pas".encode('utf-8'))
+            self.client_socket.sendall("the file doesn't exist".encode('utf-8'))
 
-    # Définit la fonction réception de fichier
+    # Défini la fonction réception de fichier
     def receive_file(self):
         self.filename = self.client_socket.recv(1024).decode('utf-8').strip()
-        self.client_socket.sendall(f"READY TO RECEIVE {self.filename}".encode('utf-8'))
+        self.client_socket.sendall(f"ready to receive {self.filename}".encode('utf-8'))
         file_size = int(self.client_socket.recv(1024).decode('utf-8').strip())
         with open(self.filename, 'wb') as file:
             bytes_received = 0
@@ -45,16 +47,19 @@ class EchoServerThread(threading.Thread):
                     break
                 file.write(data)
                 bytes_received += len(data)
-        print(f"File {self.filename} received successfully.")
-        self.messagelog = f"receive {self.filename} from {self.nom}"
+        print(f"file {self.filename} received successfully.")
+        if self.nom not in listfichier:
+            listfichier.append(self.filename)
 
+    # Permet d'écrire dans le log
     def trace(self):
         date_heure = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        # Ouvrir le fichier logs en mode "append" pour ajouter du text ('a')
+        # Ouvre le fichier logs en mode "append" pour ajouter du text ('a')
         with open('logs.txt', 'a', encoding='utf-8') as fichier:
-            # Écrire des données dans le fichier
-            fichier.write(f'{self.messagelog},{date_heure}\n')
+            # Écrit des données dans le fichier
+            fichier.write(f'{self.messagelog},{date_heure}\n\n')
 
+    # Permet d'ajouter le nom d'une personne se connectant
     def add_person(self):
         if self.nom not in listnom:
             listnom.append(self.nom)
@@ -62,6 +67,7 @@ class EchoServerThread(threading.Thread):
         if self.client_address not in listip:
             listip.append(self.client_address)
 
+    # Permet de retirer le nom d'une personne se déconnectant
     def remove_person(self):
         if self.nom in listnom:
             listnom.remove(self.nom)
@@ -69,9 +75,9 @@ class EchoServerThread(threading.Thread):
     def run(self):
         try:
             # Informe le client de la connexion
-            self.client_socket.sendall("Client connected".encode('utf-8'))
+            self.client_socket.sendall("client connected".encode('utf-8'))
 
-            # Reçois le nom du client, note la connection dans les logs et l'ajoute a la liste des perssone connecter
+            # Reçoit le nom du client, note la connection dans les logs et l'ajoute à la liste des personnes connectées
             self.nom = self.client_socket.recv(1024).decode('utf-8').strip()
             self.add_person()
             self.messagelog = self.nom + f" connected"
@@ -79,36 +85,41 @@ class EchoServerThread(threading.Thread):
 
             self.client_socket.sendall(f"connected as: {self.nom}".encode('utf-8'))
             while True:
-            # Demande au client s'il veut uploader ou télécharger un fichier
+                # Demande au client s'il veut uploader ou télécharger un fichier
                 choice = self.client_socket.recv(1024).decode('utf-8').strip()
-                if choice == "0":
+                if choice == "do":
                     self.send_file()
-                    self.messagelog = "file sended"
+                    self.messagelog = "file "+self.filename+" sent to "+self.nom
                     self.trace()
 
-                elif choice == "1":
+                elif choice == "up":
                     self.receive_file()
-                    self.messagelog = "file recived"
+                    self.messagelog = "file "+self.filename+" received from "+self.nom
                     self.trace()
 
-                elif choice == "2":
-                    self.messagelog = f"Connection with {self.nom} :{self.client_address} closed"
+                elif choice == "di":
+                    self.messagelog = f"connection with {self.nom} :{self.client_address} closed"
                     self.trace()
                     self.remove_person()
                     self.client_socket.close()
                     break
 
-                elif choice == "3":
-                    self.messagelog = f" list of connected client send to {self.nom}"
+                elif choice == "cl":
+                    self.messagelog = f"list of connected client sent to {self.nom}"
                     self.trace()
                     self.client_socket.sendall(f"{listnom}".encode('utf-8'))
 
+                elif choice == "fl":
+                    self.messagelog = f"list of file sent to {self.nom}"
+                    self.trace()
+                    self.client_socket.sendall(f"{listfichier}".encode('utf-8'))
+
         # Gère les erreurs
-        except Exception as e:
-            print(f"client deconnecter")
+        except Exception:
+            print(f"client disconnected")
 
 
-# Définit le numéro de port du serveur
+# Défini le numéro de port du serveur
 port_number = 9000
 
 # Indique que le serveur écoute les informations arrivant sur son port
@@ -116,7 +127,7 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(('localhost', port_number))
 server_socket.listen(1)
 
-print(f"Server is listening on port {port_number}")
+print(f"server is listening on port {port_number}")
 
 while True:
     client_socket, client_address = server_socket.accept()
